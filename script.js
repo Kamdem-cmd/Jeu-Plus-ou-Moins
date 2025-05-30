fbb6c080457eb53d1101b8397e0c78727b3145c2
unknown <kamdemkevin87@gmail.com>
2025-05-30 07:52:38 +0100

    Upload files

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 let min = 1;
 let max = 100;
+const commencer = document.getElementById("commencer");
+const jeu = document.getElementById("game");
+const essaie = document.getElementById("essaie");
+const valider = document.getElementById("valider");
+const commentaire = document.getElementById("commentaire");
+
+jeu.style.display = "none";
+
+function startGame(){
+  jeu.style.display = "inline";  
+  commencer.type= "reset";
+  commencer.innerHTML= "Recommencer";
+}
 let entierAleatoire = Math.floor(Math.random() * (max)) + min;
 console.log(entierAleatoire);
+commencer.addEventListener("click", startGame);
